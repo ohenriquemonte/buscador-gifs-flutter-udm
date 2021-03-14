19489db030ea11e6a1847c94d6c671b52eb4656f
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-03-14 17:40:21 -0300

    88. Criando e Trocando de Tela

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:ui';
 
+import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -118,6 +119,14 @@ class _HomePageState extends State<HomePage> {
       itemBuilder: (context, index) {
         if (_search == null || index < snapshot.data["data"].length) {
           return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GifPage(snapshot.data["data"][index]),
+                ),
+              );
+            },
             child: Image.network(
               snapshot.data["data"][index]["images"]["fixed_height"]["url"],
               height: 300.0,
