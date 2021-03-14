c0517811ff67ac3729cf893c6766b81874bbe328
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-03-14 17:14:02 -0300

    86. Grid de Gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -82,7 +82,7 @@ class _HomePageState extends State<HomePage> {
                       if (snapshot.hasError)
                         return Container();
                       else
-                        _createGifTable(context, snapshot);
+                        return _createGifTable(context, snapshot);
                   }
                 }),
           ),
@@ -91,5 +91,24 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {}
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(2),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: snapshot.data["data"].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+    );
+  }
 }
